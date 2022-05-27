beee3f44ab4c15713fe92f758fa1a713bd372b10
marceloslo <marcelosartlocatelli@gmail.com>
2022-05-27 16:40:36 -0300

    PYTHON CRINGE COM JS CRINGE NADA FUNCIONA

---
+++ TikTokCollector.py
@@ -0,0 +1,165 @@
+import selenium
+import re
+from datetime import datetime, timedelta
+import time
+import random
+from selenium import webdriver
+from selenium_stealth import stealth
+
+class TikTokCollector:
+    def __init__(self,driver):
+        stealth(driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+        )
+        self.driver=driver
+        self.url=""
+
+    #url deve ser a url de um video no formato https://www.tiktok.com/@user/video/id para o coletor de video
+    #ou https://www.tiktok.com/@user para o coletor de perfis
+    def setUrl(self,url):
+        self.url=url
+        self.driver.get(url)
+
+    #retorna dicionario com todas estatisticas visiveis do local analisado
+    def getStatistics(self):
+        return {}  
+
+    #retorna dicionario com todas estatisticas visiveis do video da url dada
+    def getStatisticsFromUrl(self,url):
+        self.setUrl(url)
+        return self.getStatistics()
+
+class VideoStatisticsCollector(TikTokCollector):
+    def __init__(self,driver):
+        super().__init__(driver)
+
+    #status 0=off, 1=on
+    def getStatistics(self):
+        if not self.__videoExists():
+            stats = {"Url":self.url,'User':"","UserId":"","Description":"","LikeCount":"","CommentCount":"","SharesCount":"","PublicationDate":"","CollectionDate":datetime.now().strftime("%Y-%m-%d"),"Status":0}
+            return stats
+        stats = {}
+        stats['Url'] = self.url
+        stats['User'] = self.__getUser()
+        stats['UserId'] = self.__getUserId()
+        stats['Description'] = self.__getDescription()
+        stats['LikeCount'] = self.__getLikes()
+        stats['CommentCount']=self.__getCommentCount()
+        stats['SharesCount']=self.__getShares()
+        stats["PublicationDate"]=self.__getPublicationDate()
+        stats["CollectionDate"] = datetime.now().strftime("%Y-%m-%d")
+        stats['Status'] = 1
+        return stats
+
+    def getMetadata(self):
+        data = self.getStatistics()
+        content = self.getContent()
+        return {**data,**content}
+        
+    def getContent(self):
+        time.sleep(2)
+        return self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/div[1]/div[1]/div/div[2]/div[1]/div/div[1]/div/video').get_attribute("src")
+
+    def __getUserId(self):
+        return self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div[1]/div[2]/div/a[2]/span[1]').text
+
+    def __getUser(self):
+        user = self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div/div[2]/div/a[2]/span[2]').text
+        idx=user[::-1].index('·')
+        user = user[:len(user)-(idx+1)]
+        return user
+
+    def __getDescription(self):
+        return self.driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div[1]/div[1]/div[2]').text
+
+    def __getLikes(self):
+        return self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div/div[1]/div[3]/button[1]/strong").text
+    
+    def __getCommentCount(self):
+        return self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div/div[1]/div[3]/button[2]/strong").text
+
+    def __getShares(self):
+        return self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div/div[1]/div[3]/button[3]/strong").text
+
+        
+    def __getPublicationDate(self):
+        date = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[1]/div[3]/div[1]/div[2]/div/a[2]/span[2]/span[2]").text
+        publication_date = date
+        if 'd' in date:
+            value = int(re.search(r'\d+', publication_date).group())
+            date_ago = (datetime.now() - timedelta(days=value)).date() 
+            publication_date = date_ago.strftime("%Y-%m-%d")
+        elif 'w' in date:
+            value = 7*int(re.search(r'\d+', publication_date).group())
+            date_ago = (datetime.now() - timedelta(days=value)).date() 
+            publication_date = date_ago.strftime("%Y-%m-%d")
+        elif 'ago' in date:
+            publication_date = datetime.now().strftime("%Y-%m-%d")
+        else:
+            try:
+                publication_date = datetime.strptime(publication_date,"%Y-%m-%d")
+                publication_date = publication_date.strftime("%Y-%m-%d")
+            except:
+                publication_date = datetime.now().strftime("%Y")+"-"+publication_date
+        return publication_date
+
+
+    #checa se há a mensagem de video removido
+    def __videoExists(self):
+        try:
+            self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[1]/div/p[1]")
+            return False
+        except:
+            return True
+
+
+class ProfileStatisticsCollector(TikTokCollector):
+    def __init__(self,driver):
+        super().__init__(driver)
+    
+    def getStatistics(self):
+        if not self.__channelExists():
+            stats = {"Url":self.url,'User':"","UserId":"","ProfileBio":"","Followers":"","Following":"","LikeCount":"","CollectionDate":datetime.now().strftime("%Y-%m-%d"),"Status":0}
+            return stats
+        stats={}
+        stats["Url"]=self.url
+        stats["User"]=self.__getProfileName()
+        stats["UserId"]=self.__getProfileId()
+        stats["ProfileBio"]=self.__getProfileBio()
+        stats["Followers"]=self.__getProfileFollowers()
+        stats["Following"]=self.__getProfileFollowing()
+        stats["LikeCount"]=self.__getProfileLikeCount()
+        stats["CollectionDate"] = datetime.now().strftime("%Y-%m-%d")
+        stats["Status"]=1
+        return stats
+
+    def __getProfileBio(self):
+        return self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div[1]/h2[2]").text
+    
+    def __getProfileFollowers(self):
+        return self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div[1]/h2[1]/div[2]/strong").text
+    
+    def __getProfileLikeCount(self):
+        return self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div[1]/h2[1]/div[3]/strong").text
+    
+    def __getProfileFollowing(self):
+        return self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div[1]/h2[1]/div[1]/strong").text
+    
+    def __getProfileId(self):
+        return self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div[1]/div[1]/div/h2").text
+    
+    def __getProfileName(self):
+        return self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div[1]/div[1]/div/h1").text
+
+    #checa se o perfil existe
+    def __channelExists(self):
+        try:
+            self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div/main/div/p[1]")
+            return False
+        except:
+            return True
